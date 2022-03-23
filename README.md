ba65cf4447ca2ceee13c6a03a33831db9a119c9d
Jiadi Su <20302010043@fudan.edu.cn>
2022-03-23 14:33:00 +0800

    add lab1-info

--- README.md
+++
@@ -1,4 +0,0 @@
-# RSIC-V_CPU
-计算机组成体系结构课程项目
-## lab1 五级流水线
-本次lab实现指令：addi xori ori andi lui jal beq ld sd add sub and or xor auipc jalr
---
+++ project-info/lab1-info/lab1第一次提交/20302010043/vsrc/pipeline/memory/memory.sv
@@ -0,0 +1,20 @@
+`ifndef __MEMORY_SV
+`define __MEMORY_SV
+
+`ifdef VERILATOR
+`include "include/common.sv"
+`include "include/pipes.sv"
+`else
+
+`endif
+
+module memory 
+    import common::*;
+    import pipes::*;(
+    
+    
+);
+    
+endmodule
+
+`endif 
